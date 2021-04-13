63bcf1763e449c45123b8a11e29c7387d9ae045e
chandan <kchandan234@gmail.com>
2021-04-13 16:12:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Provider } from "react-redux";
+import store from "./store";
+import './App.css';
+import LoginPage from './LoginPage';
+import { data } from './Data'
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super();
+    this.formValidation = this.formValidation.bind(this);
+    this.state = { isLoginPage: true }
+  }
+  formValidation = (values) => {
+    if (!(values.username === 'hruday@gmail.com' && values.password === 'hruday123')) {
+      alert("Please try with correct username and password")
+    }
+    else {
+      this.setState({ isLoginPage: false })
+    }
+  }
+
+  render() {
+    const { isLoginPage } = this.state;
+    return <Provider store={store}>
+      <div style={{ padding: 15 }}>
+        {isLoginPage ?
+          <LoginPage validateFun={this.formValidation} />
+          :
+          <div>
+            <h1 style={{textAlign:'center',borderBottom:'6px solid green'}}>Dashboard</h1>
+            <table width="100%">
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+              </tr>
+              {data.map(users =>
+                <tr>
+                  <td>{users.id}</td>
+                  <td>{users.name}</td>
+                  <td>{users.age}</td>
+                  <td>{users.gender}</td>
+                  <td>{users.email}</td>
+                  <td>{users.phoneNo}</td>
+                </tr>
+              )}
+            </table>
+          </div>
+        }
+      </div>
+    </Provider>
+  }
+}
+
+export default App;
---
+++ src/LoginPage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+import './App.css';
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div className="form">
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} className="form-input" />
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
+
+const LoginPage = props => {
+  console.log("test comment")
+  const { error, handleSubmit, submitting } = props
+  return (
+    <form onSubmit={handleSubmit(props.validateFun)} className="login-form">
+      <h2 className="login-header" > Login </h2>
+      <Field
+        name="username"
+        type="text"
+        component={renderField}
+        label="Username"
+        
+      />
+      
+      <Field
+        name="password"
+        type="password"
+        component={renderField}
+        label="Password"
+      />
+      {error && <strong>{error}</strong>}
+      <div>
+        <button type="submit" className="button" disabled={submitting}>
+          Sign in
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default reduxForm({
+  form: 'loginPageValidation'
+})(LoginPage)
